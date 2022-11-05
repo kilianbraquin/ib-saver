8d9ec63297f2e7d9f6a49dfbfe323c3c7c368d7e
Kilian Braquin <kbraquin@indiebaie.com>
2022-11-05 11:15:38 +0100

    feat: add portfolio link in footer

--- src/components/atoms/Footer.tsx
+++ src/components/atoms/Footer.tsx
@@ -5,13 +5,16 @@ export const Footer: FC = () => {
   return (
     <footer className="flex flex-col items-center p-4 space-y-1 text-body-text-light dark:text-body-text-dark">
       <div>
-        <Link className="font-bold text-primary" href="#">
-          Version Française
+        <Link className="font-bold text-primary cursor-not-allowed" href="#">
+          English Version
         </Link>
       </div>
       <div>
         Crée par{" "}
-        <Link className="font-bold text-primary" href="#">
+        <Link
+          className="font-bold text-primary"
+          href="https://www.kbraquin.com/"
+        >
           Kilian Braquin
         </Link>
       </div>
