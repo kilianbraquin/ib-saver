fc75c8c313a826244ee0acfdea8c72d225e3f09b
Kilian Braquin <kbraquin@indiebaie.com>
2022-11-06 09:09:45 +0100

    fix: remove translation link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,6 @@ export const Footer: FC = () => {
   return (
     <footer className="flex flex-col items-center p-4 space-y-1 text-body-text-light dark:text-body-text-dark">
       <div>
-        <Link className="font-bold text-primary cursor-not-allowed" href="#">
-          English Version
-        </Link>
-      </div>
-      <div>
         Crée par{" "}
         <Link
           className="font-bold text-primary"
