61112e08c6f297afa37dd86ce117e5f0b69f38c9
Kilian Braquin <kbraquin@indiebaie.com>
2022-11-06 08:56:52 +0100

    feat: error page

---
+++ src/components/templates/ErrorTemplate.tsx
@@ -0,0 +1,6 @@
+import { ErrorPresentation } from "@/components/organisms/ErrorPresentation";
+import { FC } from "react";
+
+export const ErrorTemplate: FC = () => {
+  return <ErrorPresentation />;
+};
---
+++ src/pages/404.tsx
@@ -0,0 +1,49 @@
+import { ErrorTemplate } from "@/components/templates/ErrorTemplate";
+import { GetStaticProps, NextPage } from "next";
+import { NextSeo } from "next-seo";
+
+const HomePage: NextPage = (props) => {
+  return (
+    <>
+      <NextSeo
+        title="IB Saver - Téléchargement de médias Twitter"
+        description="Recherchez vos tweets préférés et téléchargez leurs médias (images, vidéos) en qualité optimale"
+        canonical="https://www.ibsaver.com"
+        openGraph={{
+          title: "IB Saver - Téléchargement de médias Twitter",
+          description:
+            "Recherchez vos tweets préférés et téléchargez leurs médias (images, vidéos) en qualité optimale",
+          url: "https://www.ibsaver.com",
+          site_name: "IB Saver",
+          type: "website",
+          locale: "fr_FR",
+          images: [
+            {
+              url: "https://www.ibsaver.com/images/ibsaver_illu.png",
+              width: 1200,
+              height: 630,
+              alt: "Og Image Alt",
+              type: "image/jpeg",
+            },
+          ],
+        }}
+        twitter={{
+          site: "@ib_saver",
+          cardType: "summary_large_image",
+        }}
+        noindex={true}
+      />
+      <ErrorTemplate {...props} />
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  // const { title, introduction } = await getHomePage();
+
+  return {
+    props: {},
+  };
+};
+
+export default HomePage;
