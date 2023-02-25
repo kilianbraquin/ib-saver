c306a8000ce47081dfe9d1b2688ac380233c5cc6
Kilian Braquin <kbraquin@indiebaie.com>
2023-02-25 12:33:45 +0100

    chore: remove directus env variables

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,5 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NEXT_PUBLIC_DIRECTUS_CMS_URL: string;
     NEXT_PUBLIC_FATHOM_SCRIPT_URL: string;
     NEXT_PUBLIC_FATHOM_SITE_ID: string;
     NEXT_PUBLIC_DOMAIN_NAME: string;
