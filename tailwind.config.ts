df3f0316113268539711dcb892d83c7a8d07874d
Kilian Braquin <kbraquin@kilians-mbp.home>
2023-04-09 16:35:46 +0200

    refactor: define tailwind config with typescript

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import {Config} from "tailwindcss";
+import {tailwindColors} from "./src/lib/tailwind/config";
+
+const config: Config = {
+  content: ["./src/**/*.tsx"],
+  darkMode: "class",
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
+    colors: tailwindColors,
+    fontFamily: {
+      main: "zeitung-micro, sans-serif",
+      logo: "bungee, sans-serif",
+    },
+    boxShadow: {
+      button: "2px 2px 0 0 #000",
+      box: "4px 4px 0 0 #000",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config
